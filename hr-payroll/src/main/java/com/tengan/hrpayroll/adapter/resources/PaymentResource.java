e605056190f6141c694def87039937de708bd247
Lucas Tengan <lucastengan@usp.br>
2021-06-08 16:12:15 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/tengan/hrpayroll/adapter/resources/PaymentResource.java
@@ -0,0 +1,28 @@
+package com.tengan.hrpayroll.adapter.resources;
+
+import com.tengan.hrpayroll.application.services.PaymentService;
+import com.tengan.hrpayroll.domain.entities.Payment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+
+    private final PaymentService paymentService;
+
+    public PaymentResource(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping("/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = paymentService.getPayment(workerId, days);
+
+        return new ResponseEntity<>(payment, HttpStatus.OK);
+    }
+}
