9061be2828bf55f9ce06db6d7787bb93c55227e9
Lucas Tengan <lucastengan@usp.br>
2021-06-09 15:55:38 -0300

    Configuring Eureka server

--- hr-eureka-server/src/main/java/com/tengan/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/tengan/hreurekaserver/HrEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.tengan.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer         // Eureka serve pra fazer o controle/registro dos microsserviços, gerar portas, chamando os microsserviços pelo nome e não endereço
 public class HrEurekaServerApplication {
 
     public static void main(String[] args) {
