23f722830ddf1bb2b78b51c2181c66008fac35c4
Lucas Tengan <lucastengan@usp.br>
2021-06-11 15:50:58 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/tengan/hrworker/adapter/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/tengan/hrworker/adapter/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.tengan.hrworker.domain.entities.Worker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/workers")
+@RefreshScope
 public class WorkerResource {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkerResource.class);
